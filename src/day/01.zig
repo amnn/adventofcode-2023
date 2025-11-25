5dd887bb39e36c919f793e76f3c4cc0017b85d56
Ashok Menon <ashok@mystenlabs.com>
2025-11-25 00:05:40 +0000

    day-01: part 1

--- src/day/01.zig
+++ src/day/01.zig
@@ -1,5 +1,33 @@
 const std = @import("std");
 
+const lib = @import("lib");
+
+const mem = std.mem;
+
+const File = std.fs.File;
+const Limit = std.Io.Limit;
+const Writer = std.Io.Writer;
+
+const Error = error{
+    BadInput,
+};
+
 pub fn main() !void {
-    std.debug.print("Hello, {s}\n", .{"world!"});
+    var line: [4096]u8 = undefined;
+    var input: [4096]u8 = undefined;
+    var reader = File.stdin().reader(&input);
+    const stdin = &reader.interface;
+
+    var part1: u64 = 0;
+    while (try lib.readLine(stdin, &line)) |l| {
+        part1 += try calibrationPart1(l);
+    }
+
+    std.debug.print("Part 1: {d}\n", .{part1});
+}
+
+fn calibrationPart1(line: []const u8) !u8 {
+    const fst = mem.indexOfAny(u8, line, "0123456789") orelse return error.BadInput;
+    const lst = mem.lastIndexOfAny(u8, line, "0123456789") orelse return error.BadInput;
+    return (line[fst] - '0') * 10 + (line[lst] - '0');
 }
--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,15 @@
 const std = @import("std");
+
+const Limit = std.Io.Limit;
+const Reader = std.Io.Reader;
+const Writer = std.Io.Writer;
+
+/// Read a line from `Reader` `r` into fixed-size `buffer`, returning `null`
+/// after reading the first empty line, and an error if the read fails.
+pub fn readLine(r: *Reader, buffer: []u8) !?[]u8 {
+    var w = Writer.fixed(buffer);
+    if (0 == try r.streamDelimiterEnding(&w, '\n')) return null;
+    _ = try r.discard(Limit.limited(1));
+
+    return w.buffered();
+}
