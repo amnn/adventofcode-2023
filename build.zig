c7ef090171ec6e35d4cd642d4aa8aa6278c3dc8e
Ashok Menon <ashok@mystenlabs.com>
2025-12-01 18:59:06 +0000

    chore: factor out libadvent
    It's now at https://github.com/amnn/libadvent-zig

--- build.zig
+++ build.zig
@@ -4,11 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Library shared between all solutions.
-    const lib = b.addModule("lib", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
+    const lib = b.dependency("libadvent", .{}).module("libadvent");
 
     for (1..4) |i| {
         const name = std.fmt.allocPrint(b.allocator, "day{d:02}", .{i}) catch unreachable;
@@ -39,15 +35,4 @@ pub fn build(b: *std.Build) void {
             run.addArgs(args);
         }
     }
-
-    // Unit tests for the shared library.
-    const tests = b.addTest(.{
-        .root_module = lib,
-    });
-
-    // A run step that will run the test executable.
-    const run_tests = b.addRunArtifact(tests);
-
-    const step_test = b.step("test", "Run unit tests");
-    step_test.dependOn(&run_tests.step);
 }
